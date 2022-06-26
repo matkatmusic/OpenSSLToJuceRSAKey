96c68bbdfc06b6b65197e991b27820b61c6f1c9e
Charles Schiermeyer <matkatmusic@gmail.com>
2022-06-26 13:47:25 -0700

    Commented everything out so I can look for what I need prior to conversion

--- ANS1Parser/Forge/Util.cpp
+++ ANS1Parser/Forge/Util.cpp
@@ -9,3 +9,13 @@
 */
 
 #include "Util.h"
+
+/*
+ need:
+ [x] util.isArray
+ [x] util.createBuffer -> toHex()
+ [x] util.bytesToHex
+ [x] util.decodeUtf8 -> decodeURIComponent() -> escape()
+ [] util.ByteBuffer -> ByteStringBuffer
+ [x] util.ByteStringBuffer.prototype.toHex
+ */
